d5851eb281a544e7b858c8e8945c9cd37918b390
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2026-04-25 19:40:03 +0900

    feat: vec2, vec3

--- src/ofxSol2Lua.cpp
+++ src/ofxSol2Lua.cpp
@@ -29,10 +29,113 @@ std::shared_ptr<sol::state> ofxSol2Lua::getLuaState(){
 
 void ofxSol2Lua::setBindings(const std::shared_ptr<sol::state>& lua){
     setOFGeneratedBindings(lua);
-    // setTypeBindings(lua);
-    // setConstBindings(lua);
+    setTypeBindings(lua);
+    setConstBindings(lua);
     // setColorConstBindings(lua);
     setMathBindings(lua);
+}
+
+void ofxSol2Lua::setConstBindings(const std::shared_ptr<sol::state>& lua){
+    auto&& l = *lua;
+    l["PI"] = PI;
+    l["HALF_PI"] = HALF_PI;
+    l["TWO_PI"] = TWO_PI;
+    l["FOUR_PI"] = FOUR_PI;
+}
+
+void ofxSol2Lua::setTypeBindings(const std::shared_ptr<sol::state>& lua){
+    using Vec2 = ofVec2f;
+    using Vec3 = ofVec3f;
+    using Vec4 = ofVec4f;
+    using Mat4 = ofMatrix4x4;
+
+    sol::usertype<Vec2> vec2_type = lua->new_usertype<Vec2>("ofVec2f",
+        sol::constructors<Vec2(), Vec2(float, float), Vec2(const Vec2&)>(),
+        sol::meta_function::addition,
+        sol::overload(
+           [](const Vec2& a, const Vec2& b){ return a + b; },
+           [](const Vec2& a, float b){ return a + b; }
+        ),
+        sol::meta_function::subtraction,
+        sol::overload(
+           [](const Vec2& a, const Vec2& b){ return a - b; },
+           [](const Vec2& a, float b){ return a - b; }
+        ),
+        sol::meta_function::multiplication,
+        sol::overload(
+           [](const Vec2& a, const Vec2& b){ return a * b; },
+           [](const Vec2& a, float b){ return a * b; }
+        ),
+        sol::meta_function::division,
+        sol::overload(
+           [](const Vec2& a, const Vec2& b){ return a / b; },
+           [](const Vec2& a, float b){ return a / b; }
+        )
+    );
+    vec2_type["x"] = &Vec2::x;
+    vec2_type["y"] = &Vec2::y;
+
+    vec2_type["length"] = &Vec2::length;
+    vec2_type["lengthSquared"] = &Vec2::lengthSquared;
+    vec2_type["normalized"] = &Vec2::normalized;
+    vec2_type["normalize"] = &Vec2::normalize;
+    vec2_type["limit"] = &Vec2::limit;
+    vec2_type["dot"] = &Vec2::dot;
+    vec2_type["distance"] = &Vec2::distance;
+    vec2_type["distanceSquared"] = &Vec2::distanceSquared;
+    vec2_type["angle"] = sol::overload(
+        [](Vec2& p, const Vec2& v){ return p.angle(v); }
+    );
+    vec2_type["rotated"] = sol::overload(
+        [](Vec2& p, float a){ return p.rotated(a); },
+        [](Vec2& p, float a, const Vec2& v){ return p.rotated(a, v); }
+    );
+    vec2_type["rotate"] = sol::overload(
+        [](Vec2& p, float a){ return p.rotate(a); },
+        [](Vec2& p, float a, const Vec2& v){ return p.rotate(a, v); }
+    );
+    vec2_type["getRotated"] = sol::overload(
+        [](Vec2& p, float a){ return p.getRotated(a); },
+        [](Vec2& p, float a, const Vec2& v){ return p.getRotated(a, v); }
+    );
+    vec2_type["perpendicular"] = &Vec2::perpendicular;
+
+    sol::usertype<Vec3> vec3_type = lua->new_usertype<Vec3>("ofVec3f",
+        sol::constructors<Vec3(), Vec3(float, float, float), Vec3(const Vec2&), Vec3(const Vec3&)>(),
+        sol::meta_function::addition,
+        sol::overload(
+           [](const Vec3& a, const Vec3& b){ return a + b; },
+           [](const Vec3& a, float b){ return a + b; }
+        ),
+        sol::meta_function::subtraction,
+        sol::overload(
+           [](const Vec3& a, const Vec3& b){ return a - b; },
+           [](const Vec3& a, float b){ return a - b; }
+        ),
+        sol::meta_function::multiplication,
+        sol::overload(
+           [](const Vec3& a, const Vec3& b){ return a * b; },
+           [](const Vec3& a, float b){ return a * b; }
+        ),
+        sol::meta_function::division,
+        sol::overload(
+           [](const Vec3& a, const Vec3& b){ return a / b; },
+           [](const Vec3& a, float b){ return a / b; }
+        )
+    );
+    vec3_type["x"] = &Vec3::x;
+    vec3_type["y"] = &Vec3::y;
+    vec3_type["z"] = &Vec3::z;
+
+    vec3_type["length"] = &Vec3::length;
+    vec3_type["lengthSquared"] = &Vec3::lengthSquared;
+    vec3_type["normalized"] = &Vec3::normalized;
+    vec3_type["normalize"] = &Vec3::normalize;
+    vec3_type["limit"] = &Vec3::limit;
+    vec3_type["dot"] = &Vec3::dot;
+    vec3_type["cross"] = &Vec3::cross;
+    vec3_type["distance"] = &Vec3::distance;
+    vec3_type["distanceSquared"] = &Vec3::distanceSquared;
 }
 
 void ofxSol2Lua::setMathBindings(const std::shared_ptr<sol::state>& lua){
